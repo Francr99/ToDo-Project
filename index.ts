d40762b5eefe5b779ab7130bc5c6c54096bbd68d
trelaj <112722527+trelaj@users.noreply.github.com>
2023-04-20 22:13:08 -0400

    added cors

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import tasksRouter from "./routes/tasks";
+import cors from "cors";
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", tasksRouter);
 // app.use("/", Router);
